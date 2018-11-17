53edb7469ee85394b430220b47012ad1b13218cf
Mohammed <mohammedmohsin68217@gmail.com>
2018-11-17 14:24:21 +0200

    Add calculatoion logic

--- Window-shopper/Controller/MainViewController.swift
+++ Window-shopper/Controller/MainViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class ViewController: UIViewController {
 
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+        wageTextField.text = ""
+        priceTextField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calculateButton.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitleColor(UIColor.white, for: .normal)
+        calculateButton.addTarget(self, action: #selector(ViewController.calculate), for: .touchUpInside)
+        
+        wageTextField.inputAccessoryView = calculateButton
+        priceTextField.inputAccessoryView = calculateButton
+        
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+    }
+    
+    @objc func calculate() {
+        if let wageText = wageTextField.text, let priceText = priceTextField.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                
+                view.endEditing(true)
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+                resultLabel.isHidden = false
+                hoursLabel.isHidden = false
+                
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
